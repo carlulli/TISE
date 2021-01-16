c78997da5e55f123f5acde38767d352804c28ada
Adronis <astistroth@gmail.com>
2021-01-16 16:09:27 +0100

    Description for compile.sh

--- compile.sh
+++ compile.sh
@@ -1,3 +1,11 @@
+################################################################################
+# This script can be used to compile the main.c when inside TISE directory
+# It creates an executable main.o that needs to be called with the parameter
+# for the TISE simulation
+# like this:
+# ./main.o [int NUM] [double mass] [int potential number] [double tol] [double res]
+################################################################################
+
 IDIR="./include"
 MDIR="./modules"
 
