afc5c9bed0cdcb052790b941959f6b1805d2606e
Franciscon <franciscocrespo95@googlemailcom>
2021-01-20 18:59:05 +0100

    for compile.sh -Wall was added also changed conjugate gradient and its testing

--- compile.sh
+++ compile.sh
@@ -10,9 +10,9 @@ MDIR="./modules"
 TDIR="./test"
 SDIR="./scripts"
 
-gcc -o main.o -I ${IDIR} ${MDIR}/geometry.c ${MDIR}/hamiltonian.c ${MDIR}/powermethod.c ${MDIR}/observables.c ${MDIR}/linearalgebra.c ${MDIR}/conjugategradient.c main.c -lm
-gcc -o ${TDIR}/analytic.o -I ${IDIR} ${MDIR}/geometry.c ${MDIR}/hamiltonian.c ${MDIR}/linearalgebra.c ${MDIR}/assert.c ${TDIR}/analytic.c -lm
-gcc -o ${TDIR}/geometrytest.o -I ${IDIR} ${MDIR}/geometry.c ${TDIR}/geometrytest.c -lm
-gcc -o ${TDIR}/hermitian.o -I ${IDIR} ${MDIR}/geometry.c ${MDIR}/hamiltonian.c ${MDIR}/wavefunction.c ${MDIR}/linearalgebra.c ${MDIR}/assert.c ${TDIR}/hermitian.c -lm
-gcc -o ${TDIR}/power_algo.o -I ${IDIR} ${MDIR}/geometry.c ${MDIR}/powermethod.c ${MDIR}/assert.c  ${MDIR}/linearalgebra.c  ${TDIR}/power_algo.c -lm
-gcc -o ${TDIR}/test_conjugate.o ${TDIR}/test_conjugate.c -lm
+gcc -Wall -o main.o -I ${IDIR} ${MDIR}/geometry.c ${MDIR}/hamiltonian.c ${MDIR}/powermethod.c ${MDIR}/observables.c ${MDIR}/linearalgebra.c ${MDIR}/conjugategradient.c main.c -lm
+gcc -Wall -o ${TDIR}/analytic.o -I ${IDIR} ${MDIR}/geometry.c ${MDIR}/hamiltonian.c ${MDIR}/linearalgebra.c ${MDIR}/assert.c ${TDIR}/analytic.c -lm
+gcc -Wall -o ${TDIR}/geometrytest.o -I ${IDIR} ${MDIR}/geometry.c ${TDIR}/geometrytest.c -lm
+gcc -Wall -o ${TDIR}/hermitian.o -I ${IDIR} ${MDIR}/geometry.c ${MDIR}/hamiltonian.c ${MDIR}/wavefunction.c ${MDIR}/linearalgebra.c ${MDIR}/assert.c ${TDIR}/hermitian.c -lm
+gcc -Wall -o ${TDIR}/power_algo.o -I ${IDIR} ${MDIR}/geometry.c ${MDIR}/powermethod.c ${MDIR}/assert.c  ${MDIR}/linearalgebra.c  ${TDIR}/power_algo.c -lm
+gcc -Wall -o ${TDIR}/test_conjugate.o -I ${IDIR} ${MDIR}/geometry.c ${MDIR}/assert.c ${MDIR}/linearalgebra.c ${MDIR}/conjugategradient.c ${TDIR}/test_conjugate.c -lm
