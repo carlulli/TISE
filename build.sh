dfaa5f555187efe5a346ae0dc3829008f2124129
Adronis <astistroth@gmail.com>
2021-01-22 10:04:13 +0100

    quickfix-build.sh

--- build.sh
+++ build.sh
@@ -16,9 +16,9 @@ SDIR="./scripts"
 # -I : include stuff
 # -Wall : higher warning sensitivity of compiler
 ################################################################################
-gcc -o main.out -I ${IDIR} ${MDIR}/geometry.c ${MDIR}/hamiltonian.c ${MDIR}/powermethod.c ${MDIR}/observables.c ${MDIR}/linearalgebra.c ${MDIR}/conjugategradient.c main.c -lm -Wall
-gcc -o ${TDIR}/analytic.out -I ${IDIR} ${MDIR}/geometry.c ${MDIR}/hamiltonian.c ${MDIR}/linearalgebra.c ${MDIR}/assert.c ${TDIR}/analytic.c -lm -Wall
-gcc -o ${TDIR}/geometrytest.out -I ${IDIR} ${MDIR}/geometry.c ${TDIR}/geometrytest.c -lm -Wall
-gcc -o ${TDIR}/hermitian.out -I ${IDIR} ${MDIR}/geometry.c ${MDIR}/hamiltonian.c ${MDIR}/wavefunction.c ${MDIR}/linearalgebra.c ${MDIR}/assert.c ${TDIR}/hermitian.c -lm -Wall
-gcc -o ${TDIR}/power_algo.out -I ${IDIR} ${MDIR}/geometry.c ${MDIR}/powermethod.c ${MDIR}/assert.c  ${MDIR}/linearalgebra.c  ${TDIR}/power_algo.c -lm -Wall
-gcc -o ${TDIR}/test_conjugate.out -I ${TDIR}/test_conjugate.c -lm -Wall
+gcc -Wall -o main.out -I ${IDIR} ${MDIR}/geometry.c ${MDIR}/hamiltonian.c ${MDIR}/powermethod.c ${MDIR}/observables.c ${MDIR}/linearalgebra.c ${MDIR}/conjugategradient.c main.c -lm
+gcc -Wall -o ${TDIR}/analytic.out -I ${IDIR} ${MDIR}/geometry.c ${MDIR}/hamiltonian.c ${MDIR}/linearalgebra.c ${MDIR}/assert.c ${TDIR}/analytic.c -lm
+gcc -Wall -o ${TDIR}/geometrytest.out -I ${IDIR} ${MDIR}/geometry.c ${TDIR}/geometrytest.c -lm
+gcc -Wall -o ${TDIR}/hermitian.out -I ${IDIR} ${MDIR}/geometry.c ${MDIR}/hamiltonian.c ${MDIR}/wavefunction.c ${MDIR}/linearalgebra.c ${MDIR}/assert.c ${TDIR}/hermitian.c -lm
+gcc -Wall -o ${TDIR}/power_algo.out -I ${IDIR} ${MDIR}/geometry.c ${MDIR}/powermethod.c ${MDIR}/assert.c  ${MDIR}/linearalgebra.c  ${TDIR}/power_algo.c -lm
+gcc -Wall -o ${TDIR}/test_conjugate.o -I ${IDIR} ${MDIR}/geometry.c ${MDIR}/assert.c ${MDIR}/linearalgebra.c ${MDIR}/conjugategradient.c ${TDIR}/test_conjugate.c -lm
